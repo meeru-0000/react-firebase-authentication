5d208a6c06272fb05f28f0b0fb0ffd75f911bd5a
meeru-0000 <nameerahmad02@gmail.com>
2022-05-26 02:40:28 +0500

    add

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXFme_oTPlrFmWIm707ac6Z2qawoTvf2k",
-  authDomain: "authentication-tutorial-ec556.firebaseapp.com",
-  projectId: "authentication-tutorial-ec556",
-  storageBucket: "authentication-tutorial-ec556.appspot.com",
-  messagingSenderId: "945663559539",
-  appId: "1:945663559539:web:51b9786fd75d399bf86706",
-  measurementId: "G-22FP5PHT1F",
+  apiKey: "AIzaSyDOfw0wjCkfojC02Nmk0IGU_455iUfvTp8",
+
+  authDomain: "getsasta-f23f2.firebaseapp.com",
+
+  projectId: "getsasta-f23f2",
+
+  storageBucket: "getsasta-f23f2.appspot.com",
+
+  messagingSenderId: "953226231623",
+
+  appId: "1:953226231623:web:eedb2dad4b587504609662",
+
+  measurementId: "G-VJ46PEW6KF"
+
+
 };
 
 const app = initializeApp(firebaseConfig);
